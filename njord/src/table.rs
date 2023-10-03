2e2524abc76baa664fb5fd1ce11d415f724090de
mjovanc <mjovanc@icloud.com>
2023-10-03 22:34:08 +0200

    Adding documentation to Table trait

--- njord/src/table.rs
+++ njord/src/table.rs
@@ -4,15 +4,20 @@ use std::collections::HashMap;
 ///
 /// It is used for structs that want need the behaviour of an SQL Table.
 pub trait Table {
+    /// Get the name of the table.
+    ///
+    /// Returns a reference to a string representing the name of the table.
     fn get_name(&self) -> &str;
-    fn get_columns(&self) -> &HashMap<String, String>;
-    fn get_column_fields(&self) -> Vec<String>;
-}
 
-/// The TableDefinition struct.
-///
-/// It is used for implementing Table trait.
-pub struct TableDefinition {
-    pub name: String,
-    pub columns: HashMap<String, String>,
+    /// Get the columns of the table.
+    ///
+    /// Returns a reference to a `HashMap` where the keys are column names,
+    /// and the values are column types represented as strings.
+    fn get_columns(&self) -> &HashMap<String, String>;
+
+    /// Get the names of the columns.
+    ///
+    /// Returns a `Vec<String>` containing the names of the columns in the same order
+    /// as they appear in the table.
+    fn get_column_fields(&self) -> Vec<String>;
 }
