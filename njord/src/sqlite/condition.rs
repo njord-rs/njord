a18c211802a4408515c3660571dd3e9dcea54de1
mjovanc <mjovanc@icloud.com>
2023-11-29 00:04:20 +0100

    Adding HAVING clause

--- njord/src/sqlite/condition.rs
+++ njord/src/sqlite/condition.rs
@@ -7,21 +7,60 @@ pub enum Condition {
     Ge(String, String),
     And(Box<Condition>, Box<Condition>),
     Or(Box<Condition>, Box<Condition>),
-    Comparison(String, String, String),
 }
 
 impl Condition {
+
+    fn is_numeric(value: &str) -> bool {
+        value.parse::<f64>().is_ok() || value.parse::<i64>().is_ok()
+    }
+
     pub fn build(&self) -> String {
         match self {
-            Condition::Eq(column, value) => format!("{} = '{}'", column, value),
-            Condition::Ne(column, value) => format!("{} <> '{}'", column, value),
-            Condition::Lt(column, value) => format!("{} < '{}'", column, value),
-            Condition::Gt(column, value) => format!("{} > '{}'", column, value),
-            Condition::Le(column, value) => format!("{} <= '{}'", column, value),
-            Condition::Ge(column, value) => format!("{} >= '{}'", column, value),
+            Condition::Eq(column, value) => {
+                if Condition::is_numeric(value) {
+                    format!("{} = {}", column, value)
+                } else {
+                    format!("{} = '{}'", column, value)
+                }
+            }
+            Condition::Ne(column, value) => {
+                if Condition::is_numeric(value) {
+                    format!("{} <> {}", column, value)
+                } else {
+                    format!("{} <> '{}'", column, value)
+                }
+            }
+            Condition::Lt(column, value) => {
+                if Condition::is_numeric(value) {
+                    format!("{} < {}", column, value)
+                } else {
+                    format!("{} < '{}'", column, value)
+                }
+            }
+            Condition::Gt(column, value) => {
+                if Condition::is_numeric(value) {
+                    format!("{} > {}", column, value)
+                } else {
+                    format!("{} > '{}'", column, value)
+                }
+            }
+            Condition::Le(column, value) => {
+                if Condition::is_numeric(value) {
+                    format!("{} <= {}", column, value)
+                } else {
+                    format!("{} <= '{}'", column, value)
+                }
+            }
+            Condition::Ge(column, value) => {
+                if Condition::is_numeric(value) {
+                    format!("{} >= {}", column, value)
+                } else {
+                    format!("{} >= '{}'", column, value)
+                }
+            }
             Condition::And(left, right) => format!("({}) AND ({})", left.build(), right.build()),
             Condition::Or(left, right) => format!("({}) OR ({})", left.build(), right.build()),
-            Condition::Comparison(column, op, value) => format!("{} {} '{}'", column, op, value),
         }
     }
 }
