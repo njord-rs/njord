00c77e3c3f5a535ae48c08ee1a2dbec0502ab9b5
appelskrutt34 <ebba1430@hotmail.com>
2023-12-02 16:17:54 +0100

    Added tailwind and navbar

---
+++ website/src/lib/components/GithubBar.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Github from "$lib/components/icons/Github.svelte"
+    import Star from "$lib/components/icons/Star.svelte"
+    import Commit from "$lib/components/icons/Commit.svelte"
+    export let repo;
+  </script>
+  
+<div class="container flex-row items-center flex gap-6 justify-center py-4">
+    <a href="https://github.com/mjovanc/njord"> <Github/> </a>
+    <a class="hover:underline flex flex-row" href={repo.latestVersion.path}>v {repo.latestVersion.version}</a>
+    <a class="hover:underline flex flex-row justify-center items-center gap-1" href={repo.stargazers.path}> <Star></Star> {repo.stargazers.stars}</a>
+    <a class="hover:underline flex flex-row justify-center items-center gap-1" href={repo.latestCommit.path}> <Commit></Commit> latest {repo.latestCommit.date}</a>
+
+    <div class="flex flex-row">
+      {#each repo.contributors as contributor}
+      <a class="-m-1" href={contributor.html_url}><img class="rounded-full hover:border-zinc-100 hover:border" width="32px" src={contributor.avatar_url} alt=""></a>
+    {/each}
+    </div>
+   
+
+</div>
---
+++ website/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      'Secondary': ['Barlow Condensed','sans-serif'],
+      'Primary': ['Manrope','sans-serif']
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
