5fc84b28a83208866bba8726eecafeecb9c96e1e
appelskrutt34 <ebba1430@hotmail.com>
2023-12-02 19:27:27 +0100

    test

--- website/src/lib/components/GithubBar.svelte
+++ website/src/lib/components/GithubBar.svelte
@@ -7,11 +7,12 @@
   
 <div class="container flex-row items-center flex gap-6 justify-center py-4">
     <a href="https://github.com/mjovanc/njord"> <Github/> </a>
-    <a class="hover:underline flex flex-row" href={repo.latestVersion.path}>v {repo.latestVersion.version}</a>
-    <a class="hover:underline flex flex-row justify-center items-center gap-1" href={repo.stargazers.path}> <Star></Star> {repo.stargazers.stars}</a>
-    <a class="hover:underline flex flex-row justify-center items-center gap-1" href={repo.latestCommit.path}> <Commit></Commit> latest {repo.latestCommit.date}</a>
+    <a class="hover:underline flex flex-row" href={repo.latestVersion.path}>version: {repo.latestVersion.version}</a>
+    <a class="hover:underline flex flex-row justify-center items-center gap-1" href={repo.stargazers.path}> <Star></Star> stars: {repo.stargazers.stars}</a>
+    <a class="hover:underline flex flex-row justify-center items-center gap-1" href={repo.latestCommit.path}> <Commit></Commit> latest: {repo.latestCommit.date}</a>
 
     <div class="flex flex-row">
+      <p class="mr-3">contributors:</p>
       {#each repo.contributors as contributor}
       <a class="-m-1" href={contributor.html_url}><img class="rounded-full hover:border-zinc-100 hover:border" width="32px" src={contributor.avatar_url} alt=""></a>
     {/each}
