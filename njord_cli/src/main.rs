ab319373aaac12fe37585c580986485ac7308daf
mjovanc <mjovanc@icloud.com>
2023-12-11 20:26:09 +0100

    Removing migration.rs

--- njord_cli/src/main.rs
+++ njord_cli/src/main.rs
@@ -1,28 +1,22 @@
-mod migration;
-use migration::Command;
+use clap::Arg;
 
 fn main() {
-    let matches = Command::new("njord")
-        .version("0.1.0")
-        .author("Marcus Cvjeticanin")
-        .about("A simple migration tool")
-        .subcommand(Command::with_name("migration"))
+    let cmd = clap::Command::new("njord")
+        .bin_name("njord")
+        .subcommand_required(true)
+        .subcommand(
+            clap::command!("migration")
+                .subcommand(clap::command!("generate").arg(Arg::new("name"))),
+        )
         .get_matches();
 
-    let command = Command::from_args(&matches);
-
-    match command {
-        Command::Generate(args) => {
-            // Implement generate command logic here
-            println!("Generating migration file...");
+    match cmd.subcommand() {
+        Some(("migration", _migration_matches)) => {
+            println!("Hello!")
         }
-        Command::Run(args) => {
-            // Implement run command logic here
-            println!("Running pending migrations...");
-        }
-        Command::Rollback(args) => {
-            // Implement rollback command logic here
-            println!("Rolling back migrations...");
+        _ => {
+            eprintln!("Invalid command. Use 'njord --help' for usage information.");
+            std::process::exit(1);
         }
     }
 }
