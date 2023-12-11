f803c118e8a032d2e7e2eb047991edde06591c38
mjovanc <mjovanc@icloud.com>
2023-12-11 21:20:28 +0100

    Adding more program arguments

--- njord_cli/src/main.rs
+++ njord_cli/src/main.rs
@@ -8,10 +8,44 @@ fn main() {
         .subcommand_required(true)
         .subcommand(
             clap::command!("migration")
-                .subcommand(clap::command!("generate").arg(Arg::new("name"))),
+                .subcommand(
+                    clap::command!("generate")
+                        .about("Generates a new migration file with the specified name.")
+                        
+                        .arg(Arg::new("env")
+                            .long("env")
+                            .help("Specifies the environment (e.g., development, test, staging, production).")
+                            .value_name("env"))
+                        
+                        .arg(Arg::new("log-level")
+                            .long("log-level")
+                            .help("Sets the logging level (e.g., standard, debug).")
+                            .value_name("log-level"))
+                        
+                        .arg(Arg::new("dry-run")
+                            .long("dry-run")
+                            .help("Simulates the migration without applying changes."))
+
+                        .arg(Arg::new("dir")
+                            .long("dir")
+                            .help("Specifies the target directory for generated migration changes.")
+                            .value_name("path"))
+                )
+                .subcommand(
+                    clap::command!("run")
+                        .about("Applies all pending migrations to the database.")
+                        .arg(Arg::new("env")
+                            .long("env")
+                            .help("Target a specific environment.")),
+                )
+                .subcommand(
+                    clap::command!("rollback")
+                        .about("Rolls back the last applied migration or to a specific version."),
+                )
         )
         .get_matches();
 
+    // match a given command/subcommand and run corresponding function
     match cmd.subcommand() {
         Some(("migration", _migration_matches)) => {
             println!("Hello!")
