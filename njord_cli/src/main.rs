393291d79c0105d98d3c4629035d658a2e5dd49b
Marcus Cvjeticanin <mjovanc@icloud.com>
2023-12-11 15:31:52 +0100

    Adding migration.rs in njord_cli

--- njord_cli/src/main.rs
+++ njord_cli/src/main.rs
@@ -1,5 +1,28 @@
+mod migration;
+use migration::Command;
+
 fn main() {
-    let _cmd = clap::Command::new("njord")
-        .bin_name("njord")
-        .subcommand_required(true);
+    let matches = Command::new("njord")
+        .version("0.1.0")
+        .author("Marcus Cvjeticanin")
+        .about("A simple migration tool")
+        .subcommand(Command::with_name("migration"))
+        .get_matches();
+
+    let command = Command::from_args(&matches);
+
+    match command {
+        Command::Generate(args) => {
+            // Implement generate command logic here
+            println!("Generating migration file...");
+        }
+        Command::Run(args) => {
+            // Implement run command logic here
+            println!("Running pending migrations...");
+        }
+        Command::Rollback(args) => {
+            // Implement rollback command logic here
+            println!("Rolling back migrations...");
+        }
+    }
 }
