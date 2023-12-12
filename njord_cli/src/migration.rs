3cef70de0f4ae0845c4c7e7fb70a3d5f618eeabd
mjovanc <mjovanc@icloud.com>
2023-12-12 16:14:51 +0100

    Adding skeleton function and new migration.rs file and updating logic in main.rs

---
+++ njord_cli/src/migration.rs
@@ -0,0 +1,11 @@
+pub fn generate() {
+    println!("Generating migration file...");
+}
+
+pub fn run() {
+    println!("Running pending migrations...");
+}
+
+pub fn rollback() {
+    println!("Rolling back migrations...");
+}
