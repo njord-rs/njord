3585eb86faf464703c27f9a3692fbfd4161f14f8
mjovanc <mjovanc@icloud.com>
2023-12-13 13:43:36 +0100

    Adding Rust docs in migration.rs

--- njord_cli/src/migration.rs
+++ njord_cli/src/migration.rs
@@ -1,18 +1,56 @@
-pub fn generate(name: Option<&String>, env: Option<&String>, dry_run: Option<&String>) {
+/// Generates migration files with the specified name, environment, and dry-run option.
+///
+/// # Arguments
+///
+/// * `name` - Optional parameter representing the name of the migration file.
+/// * `env` - Optional parameter specifying the environment (e.g., development, test, staging, production).
+/// * `dry_run` - Optional parameter indicating whether to simulate the migration without applying changes.
+///
+/// # Example
+///
+/// ```rust
+/// generate(Some("example_name"), Some("development"), Some("true"));
+/// ```
+pub fn generate(name: Option<&str>, env: Option<&str>, dry_run: Option<&str>) {
     println!(
         "Generating migration file '{:?}' env '{:?}', dry-run: {:?}",
         name, env, dry_run
     );
 }
 
-pub fn run(env: Option<&String>, log_level: Option<&String>) {
+/// Runs migration files with the specified environment and log level.
+///
+/// # Arguments
+///
+/// * `env` - Optional parameter specifying the target environment for applying migrations.
+/// * `log_level` - Optional parameter setting the logging level (e.g., standard, debug).
+///
+/// # Example
+///
+/// ```rust
+/// run(Some("production"), Some("debug"));
+/// ```
+pub fn run(env: Option<&str>, log_level: Option<&str>) {
     println!(
         "Running migration with env '{:?}' and log-level '{:?}'",
         env, log_level
     );
 }
 
-pub fn rollback(env: Option<&String>, to: Option<&String>, log_level: Option<&String>) {
+/// Rolls back migration changes to a specific version, with optional environment and log level.
+///
+/// # Arguments
+///
+/// * `env` - Optional parameter specifying the target environment for rolling back migrations.
+/// * `to` - Optional parameter setting a previous migration change to rollback to.
+/// * `log_level` - Optional parameter setting the logging level (e.g., standard, debug).
+///
+/// # Example
+///
+/// ```rust
+/// rollback(Some("development"), Some("20231204120000"), Some("info"));
+/// ```
+pub fn rollback(env: Option<&str>, to: Option<&str>, log_level: Option<&str>) {
     println!(
         "Rolling back migration with env '{:?}' to '{:?}' log_level '{:?}'",
         env, to, log_level
