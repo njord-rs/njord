0c478fa59a13106da73997ce1f07a788e6638f05
mjovanc <mjovanc@icloud.com>
2023-12-13 13:40:30 +0100

    Adding new parameters to migration.rs functions

--- njord_cli/src/migration.rs
+++ njord_cli/src/migration.rs
@@ -1,11 +1,20 @@
-pub fn generate() {
-    println!("Generating migration file...");
+pub fn generate(name: Option<&String>, env: Option<&String>, dry_run: Option<&String>) {
+    println!(
+        "Generating migration file '{:?}' env '{:?}', dry-run: {:?}",
+        name, env, dry_run
+    );
 }
 
-pub fn run() {
-    println!("Running pending migrations...");
+pub fn run(env: Option<&String>, log_level: Option<&String>) {
+    println!(
+        "Running migration with env '{:?}' and log-level '{:?}'",
+        env, log_level
+    );
 }
 
-pub fn rollback() {
-    println!("Rolling back migrations...");
+pub fn rollback(env: Option<&String>, to: Option<&String>, log_level: Option<&String>) {
+    println!(
+        "Rolling back migration with env '{:?}' to '{:?}' log_level '{:?}'",
+        env, to, log_level
+    );
 }
