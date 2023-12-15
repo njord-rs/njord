06f3e07913e0aca141b620b96b1c86127e4b4cb5
mjovanc <mjovanc@icloud.com>
2023-12-15 09:13:29 +0100

    Adding example in schema.rs

--- njord_examples/sqlite/src/schema.rs
+++ njord_examples/sqlite/src/schema.rs
@@ -1,1 +1,26 @@
+#[derive(Table, Default)]
+struct User {
+    user_id: usize,
+    username: String,
+    email: String,
+    address: String,
+}
 
+#[derive(Table, Default)]
+struct Product {
+    product_id: usize,
+    name: String,
+    description: String,
+    price: f64,
+    stock_quantity: usize,
+    category: String,
+}
+
+#[derive(Table, Default)]
+struct Order {
+    order_id: usize,
+    user_id: usize,
+    products: Vec<Product>,
+    total_cost: f64,
+    order_date: NaiveDateTime,
+}
