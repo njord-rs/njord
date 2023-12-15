8ac17981c7fd131484a8bfbd5dc2234c50344a4e
mjovanc <mjovanc@icloud.com>
2023-12-15 09:22:58 +0100

    Updating down.sql and up.sql for sqlite

--- njord_examples/sqlite/setup.sh
+++ njord_examples/sqlite/setup.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Script: setup.sh
+# Description: Build and run the njord_cli application with SQLite support.
+# Author: Marcus Cvjeticanin
+# Date: December 15, 2023
+# -----------------------------------------------------------------------------
+
 cargo build --manifest-path=../../njord_cli/Cargo.toml --no-default-features --features "sqlite"
 cargo run --manifest-path=../../njord_cli/Cargo.toml -- setup
