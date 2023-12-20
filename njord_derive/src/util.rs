6a989fc761aadac5d5c9b5964d8acf7b04a4e244
mjovanc <mjovanc@icloud.com>
2023-12-20 09:11:03 +0100

    Updating rust docs or util function in njord_derive

--- njord_derive/src/util.rs
+++ njord_derive/src/util.rs
@@ -1,4 +1,16 @@
 /// Check if the Default trait is implemented for the struct.
+///
+/// This function takes a `syn::DeriveInput` as input and checks if the Default trait is
+/// implemented for the struct. It analyzes the type parameters and their bounds to determine
+/// if the Default trait is present.
+///
+/// # Parameters
+///
+/// - `input`: A reference to the `syn::DeriveInput` representing the input struct.
+///
+/// # Returns
+///
+/// Returns `true` if the Default trait is implemented for the struct, and `false` otherwise.
 pub fn has_default_impl(input: &syn::DeriveInput) -> bool {
     if let syn::Data::Struct(_s) = &input.data {
         let generics = &input.generics;
