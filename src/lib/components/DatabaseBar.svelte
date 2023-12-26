ab784631c1b469972231a7c4f08bf1362e7e4d5d
Marcus Cvjeticanin <mjovanc@icloud.com>
2023-12-27 00:58:16 +0100

    Update DatabaseBar.svelte

--- src/lib/components/DatabaseBar.svelte
+++ src/lib/components/DatabaseBar.svelte
@@ -1,7 +1,7 @@
 <div class="container flex gap-4 md:gap-10 py-8 font-secondary ">
-    <img src="dbs/mariadb.png" alt="" class="h-16 grayscale opacity-60">
-    <img src="dbs/oracle.png" alt="" class="h-16 grayscale opacity-60">
     <img src="dbs/sqlite.png" alt="" class="h-16">
     <img src="dbs/postgre.png" alt="" class="h-16 grayscale opacity-60">
     <img src="dbs/mysql.png" alt="" class="h-16 grayscale opacity-60">
+    <img src="dbs/mariadb.png" alt="" class="h-16 grayscale opacity-60">
+    <img src="dbs/oracle.png" alt="" class="h-16 grayscale opacity-60">
 </div>
