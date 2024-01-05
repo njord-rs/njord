ecc8e66b2a5867bb7ed600d3c714d79dde5e3351
appelskrutt34 <ebba1430@hotmail.com>
2024-01-05 16:03:00 +0100

    fixed sqlserver img

--- src/lib/components/DatabaseBar.svelte
+++ src/lib/components/DatabaseBar.svelte
@@ -1,8 +1,8 @@
-<div class="container flex gap-4 md:gap-10 py-8 font-secondary ">
-    <img src="dbs/sqlite.png" alt="" class="h-16">
-    <img src="dbs/postgre.png" alt="" class="h-16 grayscale opacity-60">
-    <img src="dbs/mysql.png" alt="" class="h-16 grayscale opacity-60">
-    <img src="dbs/mariadb.png" alt="" class="h-16 grayscale opacity-60">
-    <img src="dbs/oracle.png" alt="" class="h-16 grayscale opacity-60">
-    <img src="dbs/mssql.png" alt="" class="h-16 grayscale opacity-60">
+<div class="container sm:flex gap-4 md:gap-10 py-8 font-secondary grid grid-cols-3">
+    <img src="dbs/sqlite.png" alt="" class="h-8 lg:h-16">
+    <img src="dbs/postgre.png" alt="" class="h-8 lg:h-16 grayscale opacity-50">
+    <img src="dbs/mysql.png" alt="" class="h-8 lg:h-16 grayscale opacity-50">
+    <img src="dbs/mariadb.png" alt="" class="h-8 lg:h-16 grayscale opacity-90">
+    <img src="dbs/oracle.png" alt="" class="h-8 lg:h-16 grayscale opacity-90">
+    <img src="dbs/sqlserver.webp" alt="" class="h-8 lg:h-16 grayscale opacity-50">
 </div>
