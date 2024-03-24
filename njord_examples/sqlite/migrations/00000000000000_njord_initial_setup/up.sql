18a1b95c0e9ae7e528b89b13e241d636240ced33
mjovanc <mjovanc@icloud.com>
2024-03-24 17:57:04 +0100

    Fixing get_latest_migration_version to handle error when initial table doesnt exist

--- njord_examples/sqlite/migrations/00000000000000_njord_initial_setup/up.sql
+++ njord_examples/sqlite/migrations/00000000000000_njord_initial_setup/up.sql
@@ -1,32 +1,5 @@
--- users table
-CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-    address TEXT NOT NULL
-);
-
--- products table
-CREATE TABLE products (
-    product_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    price REAL NOT NULL,
-    stock_quantity INTEGER NOT NULL,
-    category TEXT NOT NULL
-);
-
--- orders table
-CREATE TABLE orders (
-    order_id INTEGER PRIMARY KEY,
-    user_id INTEGER REFERENCES users(user_id),
-    total_cost REAL NOT NULL,
-    order_date TEXT NOT NULL
-);
-
--- order_products table
-CREATE TABLE order_products (
-    order_id INTEGER REFERENCES orders(order_id),
-    product_id INTEGER REFERENCES products(product_id),
-    PRIMARY KEY (order_id, product_id)
+CREATE TABLE IF NOT EXISTS migration_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    version TEXT NOT NULL,
+    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
