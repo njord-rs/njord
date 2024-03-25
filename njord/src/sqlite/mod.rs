962d90da6f88f1fc6eab27dc8176636eb4529763
mjovanc <mjovanc@icloud.com>
2024-03-25 10:52:07 +0100

    Reverting

--- njord/src/sqlite/mod.rs
+++ njord/src/sqlite/mod.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::env;
 
 use rusqlite::{Connection, Error};
 
@@ -12,13 +12,8 @@ pub mod query;
 
 /// Open a database connection
 pub fn open(db_name: &str) -> Result<Connection, Error> {
-    // TODO: this needs to be updated to a more elegant solution
-    let target_dir = find_target_directory(Path::new(".")).unwrap_or_else(|| {
-        println!("Target directory not found, using default '../../target'."); // TODO: use logging later
-        PathBuf::from("../../target")
-    });
-    let db_file_path = target_dir.join(db_name);
-
+    let target_dir = env::var("OUT_DIR").unwrap_or_else(|_| "../target".to_string());
+    let db_file_path = format!("{}/{}", target_dir, &db_name);
     let conn = Connection::open(db_file_path)?;
 
     Ok(conn)
