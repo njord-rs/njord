d5e6f6689c593ec26af2758dec5c039b34d0d520
mjovanc <mjovanc@icloud.com>
2024-03-25 20:47:17 +0100

    Setting up database connection

--- njord_examples/sqlite/src/main.rs
+++ njord_examples/sqlite/src/main.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use reqwest::Error;
 use reqwest::header::ACCEPT;
 use serde_json::Value;
@@ -9,13 +10,23 @@ const API_URL: &str = "https://api.nasa.gov/neo/rest/v1";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // setting up a sqlite db and connection
+    let db_relative_path = "./njord_examples/sqlite/neo.db";
+    let db_path = Path::new(&db_relative_path);
+    let conn = njord::sqlite::open(db_path);
+
     let neo = get_near_earth_objects(0, 10).await;
 
     match neo {
         Ok(data) => {
-            for obj in data["near_earth_objects"].as_array().unwrap() {
-                let near_earth_obj: NearEarthObject = serde_json::from_value(obj.clone()).unwrap();
-                println!("{:#?}", near_earth_obj);
+            match conn {
+                Ok(_) => {
+                    for obj in data["near_earth_objects"].as_array().unwrap() {
+                        let near_earth_obj: NearEarthObject = serde_json::from_value(obj.clone()).unwrap();
+                        println!("{:#?}", near_earth_obj);
+                    }
+                }
+                Err(err) => eprintln!("Error: {}", err)
             }
         }
         Err(err) => eprintln!("Error: {}", err),
@@ -26,7 +37,7 @@ async fn main() -> Result<(), Error> {
 
 async fn get_near_earth_objects(page: u32, size: u32) -> Result<Value, Error>{
     let client = reqwest::Client::new();
-    let endpoint = format!("{}/neo/browse?page=0&size=20&api_key=DEMO_KEY", API_URL);
+    let endpoint = format!("{}/neo/browse?page={}&size={}&api_key=DEMO_KEY", API_URL, page, size);
 
     let response = client
         .get(endpoint)
