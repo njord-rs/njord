3e995de589ab7444d7e0078f80a0b80df6afcab7
mjovanc <mjovanc@icloud.com>
2024-03-25 10:25:30 +0100

    Moving njord_cli and njord_examples to njord_cli branch

--- njord_cli/templates/migrations/00000000000000_njord_initial_setup/postgres/down.sql
+++
@@ -1,6 +0,0 @@
--- This file was automatically created by Njord to setup helper functions
--- and other internal bookkeeping. This file is safe to edit, any future
--- changes will be added to existing projects as new migrations.
-
-DROP FUNCTION IF EXISTS njord_manage_updated_at(_tbl regclass);
-DROP FUNCTION IF EXISTS njord_set_updated_at();
--- njord_examples/sqlite/rollback.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# -----------------------------------------------------------------------------
-# Script: rollback.sh
-# Description: Rollback migration changes to specified change with SQLite support.
-# Author: Marcus Cvjeticanin
-# Date: December 15, 2023
-# -----------------------------------------------------------------------------
-
-cargo run --manifest-path=../../njord_cli/Cargo.toml -- migration rollback --env=development --to=00000000000000
