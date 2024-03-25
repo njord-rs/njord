3108db5fcc371d70571a9883038005dbbaeb0812
mjovanc <mjovanc@icloud.com>
2024-03-25 16:11:54 +0100

    Adding rust docs

--- njord/src/sqlite/mod.rs
+++ njord/src/sqlite/mod.rs
@@ -9,14 +9,44 @@ pub use select::select;
 
 pub mod query;
 
-/// Open a database connection
+/// Open a database connection.
+///
+/// This function opens a connection to a SQLite database located at the specified path.
+///
+/// # Arguments
+///
+/// * `db_path` - A reference to the path where the SQLite database is located.
+///
+/// # Returns
+///
+/// Returns a `Result` containing a `Connection` if the operation was successful, or an `Error` if an error occurred.
+///
+/// # Errors
+///
+/// This function can return an error if:
+///
+/// * The specified database path does not exist.
+/// * There are permission issues when trying to access the database.
+/// * The database is corrupted or not a valid SQLite database.
 pub fn open(db_path: &Path) -> Result<Connection, Error> {
     let conn = Connection::open(db_path)?;
 
     Ok(conn)
 }
 
-/// Open an in-memory database connection
+/// Open an in-memory database connection.
+///
+/// This function opens a connection to an in-memory SQLite database.
+///
+/// # Returns
+///
+/// Returns a `Result` containing a `Connection` if the operation was successful, or an `Error` if an error occurred.
+///
+/// # Errors
+///
+/// This function can return an error if:
+///
+/// * There was an issue creating the in-memory database connection.
 pub fn open_in_memory() -> Result<Connection, Error> {
     let conn = Connection::open_in_memory()?;
 
