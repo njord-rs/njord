af9038cf0a480566dbd85b4961c332c3bf910c83
mjovanc <mjovanc@icloud.com>
2024-04-28 14:57:42 +0200

    Updating update.rs

--- njord/src/sqlite/update.rs
+++ njord/src/sqlite/update.rs
@@ -8,17 +8,27 @@ use rusqlite::types::Value;
 
 use crate::table::Table;
 
-pub fn update<T: Table + Default>(conn: Connection, columns: Vec<String>) -> UpdateQueryBuilder<T> {
-    UpdateQueryBuilder::new(conn, columns)
+pub fn update<T: Table + Default>(conn: Connection, table: T) -> UpdateQueryBuilder<T> {
+    UpdateQueryBuilder::new(conn, table)
 }
 
 pub struct UpdateQueryBuilder<T: Table + Default> {
     conn: Connection,
     table: Option<T>,
+    where_condition: Option<Condition>,
 }
 
 impl<T: Table + Default> UpdateQueryBuilder<T> {
-    pub fn new(conn: Connection, columns: Vec<String>) -> Self {
-        UpdateQueryBuilder { conn, table: None }
+    pub fn new(conn: Connection, table: T) -> Self {
+        UpdateQueryBuilder {
+            conn,
+            table: Some(table),
+            where_condition: None,
+        }
+    }
+
+    pub fn where_clause(mut self, condition: Condition) -> Self {
+        self.where_condition = Some(condition);
+        self
     }
 }
