48b13c894d145e19808b262b87ae77dd428c4a58
mjovanc <mjovanc@icloud.com>
2024-04-30 13:38:52 +0200

    Updating sqlite_test.rs

--- njord/tests/sqlite_test.rs
+++ njord/tests/sqlite_test.rs
@@ -122,43 +122,43 @@ fn update() {
     }
 }
 
-// #[test]
-// fn delete() {
-//     let db_relative_path = "./db/insert.db";
-//     let db_path = Path::new(&db_relative_path);
-//     let conn = sqlite::open(db_path);
+#[test]
+fn delete() {
+    let db_relative_path = "./db/insert.db";
+    let db_path = Path::new(&db_relative_path);
+    let conn = sqlite::open(db_path);
 
-//     let columns: Vec<String> = vec!["address".to_string()];
+    let columns: Vec<String> = vec!["address".to_string()];
 
-//     let condition = Condition::Eq("username".to_string(), "mjovanc".to_string());
+    let condition = Condition::Eq("username".to_string(), "mjovanc".to_string());
 
-//     let table_row: User = User {
-//         id: 0,
-//         username: "mjovanc".to_string(),
-//         email: "mjovanc@icloud.com".to_string(),
-//         address: "Some Random Address 1".to_string(),
-//     };
+    let table_row: User = User {
+        id: 0,
+        username: "mjovanc".to_string(),
+        email: "mjovanc@icloud.com".to_string(),
+        address: "Some Random Address 1".to_string(),
+    };
 
-//     let mut order = HashMap::new();
-//     order.insert(vec!["id".to_string()], "DESC".to_string());
+    let mut order = HashMap::new();
+    order.insert(vec!["id".to_string()], "DESC".to_string());
 
-//     match conn {
-//         Ok(c) => {
-//             let result = sqlite::update(c)
-//                 .from(columns)
-//                 .where_clause(condition)
-//                 .order_by(order)
-//                 .limit(4)
-//                 .offset(0)
-//                 .build();
-//             println!("{:?}", result);
-//             assert!(result.is_ok());
-//         }
-//         Err(e) => {
-//             panic!("Failed to UPDATE: {:?}", e);
-//         }
-//     }
-// }
+    match conn {
+        Ok(c) => {
+            let result = sqlite::update(c)
+                .from(User::default())
+                .where_clause(condition)
+                .order_by(order)
+                .limit(4)
+                .offset(0)
+                .build();
+            println!("{:?}", result);
+            assert!(result.is_ok());
+        }
+        Err(e) => {
+            panic!("Failed to DELETE: {:?}", e);
+        }
+    }
+}
 
 #[test]
 fn select() {
