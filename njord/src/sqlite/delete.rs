8ace6ba0577cafe8129edb7dcfd22a7971c9a235
mjovanc <mjovanc@icloud.com>
2024-04-30 13:57:55 +0200

    Implementing build method in delete.rs

--- njord/src/sqlite/delete.rs
+++ njord/src/sqlite/delete.rs
@@ -58,5 +58,61 @@ impl<T: Table + Default> DeleteQueryBuilder<T> {
         self
     }
 
-    pub fn build(self) {}
+    pub fn build(self) -> Result<(), String> {
+        let table_name = self
+            .table
+            .as_ref()
+            .map(|t| t.get_name().to_string())
+            .unwrap_or("".to_string());
+
+        // Sanitize table name from unwanted quotations or backslashes
+        let table_name_str = table_name.replace("\"", "").replace("\\", "");
+
+        let where_condition_str = if let Some(condition) = self.where_condition {
+            format!("WHERE {}", condition.build())
+        } else {
+            String::new()
+        };
+
+        let order_by_str = if let Some(order_by) = &self.order_by {
+            let order_by_str: Vec<String> = order_by
+                .iter()
+                .map(|(columns, order)| format!("{} {}", columns.join(", "), order))
+                .collect();
+            if !order_by_str.is_empty() {
+                format!("ORDER BY {}", order_by_str.join(", "))
+            } else {
+                String::new()
+            }
+        } else {
+            String::new()
+        };
+
+        let limit_str = self
+            .limit
+            .map_or(String::new(), |count| format!("LIMIT {}", count));
+        let offset_str = self
+            .offset
+            .map_or(String::new(), |offset| format!("OFFSET {}", offset));
+
+        // Construct the query based on defined variables above
+        let query = format!(
+            "UPDATE {} SET {} {} {}",
+            table_name_str,
+            where_condition_str,
+            order_by_str,
+            format!("{} {}", limit_str, offset_str),
+        );
+
+        info!("{}", query);
+        println!("{}", query);
+
+        // Prepare SQL statement
+        match self.conn.prepare(query.as_str()) {
+            Ok(_) => println!("Success!"),
+            Err(_) => eprintln!("Could not execute..."),
+        };
+
+        Ok(())
+    }
 }
