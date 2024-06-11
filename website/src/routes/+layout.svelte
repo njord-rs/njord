333881546ada56bb9ef8660f4259eab75f9ad471
mjovanc <mjovanc@icloud.com>
2024-06-11 11:06:25 +0200

    Updating website

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { inject } from '@vercel/analytics'
   import Navbar from '$lib/components/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import '../app.css';
