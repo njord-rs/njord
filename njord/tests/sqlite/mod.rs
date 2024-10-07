001bbbeeeea40405943f9ec7f3d1a4a878a4e713
Christopher Zeuch <chriszeuch.cz@gmail.com>
2024-10-07 20:52:03 +0200

    Moved structs into mod & updated test files

--- njord/tests/sqlite/mod.rs
+++ njord/tests/sqlite/mod.rs
@@ -3,3 +3,45 @@ mod insert_test;
 mod update_test;
 mod delete_test;
 mod select_test;
+
+use njord::keys::{AutoIncrementPrimaryKey, PrimaryKey};
+use njord::table::Table;
+use njord_derive::Table;
+
+#[derive(Table, Clone)]
+#[table_name = "users"]
+pub struct User {
+    pub id: AutoIncrementPrimaryKey<usize>,
+    pub username: String,
+    pub email: String,
+    pub address: String,
+}
+
+#[derive(Table)]
+#[table_name = "users"]
+pub struct UserWithSubQuery {
+    pub id: AutoIncrementPrimaryKey<usize>,
+    pub username: String,
+    pub email: String,
+    pub address: String,
+    pub additional_address: String,
+}
+
+#[derive(Table)]
+#[table_name = "categories"]
+pub struct Category {
+    pub id: PrimaryKey<usize>,
+    pub name: String,
+}
+
+#[derive(Table)]
+#[table_name = "products"]
+pub struct Product {
+    pub id: PrimaryKey<usize>,
+    pub name: String,
+    pub description: String,
+    pub price: f64,
+    pub stock_quantity: usize,
+    pub category: Category, // one-to-one relationship
+    pub discount: f64,
+}
