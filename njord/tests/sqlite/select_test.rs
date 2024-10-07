974d5545c5dbb07db8fac4b0adf5b309be5fe7af
mjovanc <mjovanc@icloud.com>
2024-10-07 13:49:53 +0200

    Adding JOIN logic

--- njord/tests/sqlite/select_test.rs
+++ njord/tests/sqlite/select_test.rs
@@ -1,10 +1,156 @@
-use super::{User, UserWithSubQuery};
 use njord::column::Column;
 use njord::condition::Condition;
-use njord::keys::AutoIncrementPrimaryKey;
+use njord::keys::{AutoIncrementPrimaryKey, PrimaryKey};
 use njord::sqlite;
 use std::collections::HashMap;
 use std::path::Path;
+
+#[derive(Table, Clone)]
+#[table_name = "users"]
+pub struct User {
+    id: AutoIncrementPrimaryKey<usize>,
+    username: String,
+    email: String,
+    address: String,
+}
+
+#[derive(Table)]
+#[table_name = "users"]
+pub struct UserWithSubQuery {
+    id: AutoIncrementPrimaryKey<usize>,
+    username: String,
+    email: String,
+    address: String,
+    additional_address: String,
+}
+
+#[derive(Table)]
+#[table_name = "categories"]
+pub struct Category {
+    id: PrimaryKey<usize>,
+    name: String,
+}
+
+#[derive(Table)]
+#[table_name = "products"]
+pub struct Product {
+    id: PrimaryKey<usize>,
+    name: String,
+    description: String,
+    price: f64,
+    stock_quantity: usize,
+    category: Category, // one-to-one relationship
+    discount: f64,
+}
+
+// #[derive(Table)]
+// #[table_name = "orders"]
+// pub struct Order {
+//     id: usize,
+//     user: User,             // one-to-one relationship
+//     products: Vec<Product>, // one-to-many relationship - populates from based on junction table (gets from macro attribute "table_name" and combines them for example, orders_products)
+//     total_cost: f64,
+// }
+
+#[test]
+fn open_db() {
+    let db_relative_path = "./db/open.db";
+    let db_path = Path::new(&db_relative_path);
+
+    let result = sqlite::open(db_path);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn insert_row() {
+    let db_relative_path = "./db/insert.db";
+    let db_path = Path::new(&db_relative_path);
+    let conn = sqlite::open(db_path);
+
+    let table_row: User = User {
+        id: AutoIncrementPrimaryKey::default(),
+        username: "mjovanc".to_string(),
+        email: "mjovanc@icloud.com".to_string(),
+        address: "Some Random Address 1".to_string(),
+    };
+
+    match conn {
+        Ok(c) => {
+            let result = sqlite::insert(c, vec![table_row]);
+            assert!(result.is_ok());
+        }
+        Err(e) => {
+            panic!("Failed to INSERT: {:?}", e);
+        }
+    }
+}
+
+#[test]
+fn update() {
+    let db_relative_path = "./db/insert.db";
+    let db_path = Path::new(&db_relative_path);
+    let conn = sqlite::open(db_path);
+
+    let columns = vec!["username".to_string()];
+
+    let condition = Condition::Eq("username".to_string(), "mjovanc".to_string());
+
+    let table_row: User = User {
+        id: AutoIncrementPrimaryKey::<usize>::new(Some(0)),
+        username: "mjovanc".to_string(),
+        email: "mjovanc@icloud.com".to_string(),
+        address: "Some Random Address 1".to_string(),
+    };
+
+    let mut order = HashMap::new();
+    order.insert(vec!["id".to_string()], "DESC".to_string());
+
+    match conn {
+        Ok(c) => {
+            let result = sqlite::update(&c, table_row)
+                .set(columns)
+                .where_clause(condition)
+                .order_by(order)
+                .limit(4)
+                .offset(0)
+                .build();
+            println!("{:?}", result);
+            assert!(result.is_ok());
+        }
+        Err(e) => {
+            panic!("Failed to UPDATE: {:?}", e);
+        }
+    }
+}
+
+#[test]
+fn delete() {
+    let db_relative_path = "./db/insert.db";
+    let db_path = Path::new(&db_relative_path);
+    let conn = sqlite::open(db_path);
+
+    let condition = Condition::Eq("address".to_string(), "Some Random Address 1".to_string());
+
+    let mut order = HashMap::new();
+    order.insert(vec!["id".to_string()], "DESC".to_string());
+
+    match conn {
+        Ok(c) => {
+            let result = sqlite::delete(c)
+                .from(User::default())
+                .where_clause(condition)
+                .order_by(order)
+                .limit(20)
+                .offset(0)
+                .build();
+            println!("{:?}", result);
+            assert!(result.is_ok());
+        }
+        Err(e) => {
+            panic!("Failed to DELETE: {:?}", e);
+        }
+    }
+}
 
 #[test]
 fn select() {
@@ -388,3 +534,37 @@ fn select_in() {
         Err(e) => panic!("Failed to SELECT: {:?}", e),
     };
 }
+
+// #[test]
+// fn select_join() {
+//     let db_relative_path = "./db/select_join.db";
+//     let db_path = Path::new(&db_relative_path);
+//     let conn = sqlite::open(db_path);
+//
+//     // Assume we have pre-inserted some data into the users and products tables
+//     let columns = vec![
+//         "users.username".to_string(),
+//         "products.name".to_string(),
+//         "products.price".to_string(),
+//     ];
+//
+//     // Assuming a hypothetical join condition: users.id = products.user_id
+//     let join_condition = Condition::Eq("users.id".to_string(), "products.user_id".to_string());
+//     match conn {
+//         Ok(c) => {
+//             let result = sqlite::select(&c, columns)
+//                 .from(User::default())
+//                 .join(JoinType::Inner, Category::default(), join_condition)
+//                 .build();
+//             match result {
+//                 Ok(r) => {
+//                     // Check the number of results and assert against expected values
+//                     assert!(!r.is_empty(), "Expected results, but got none.");
+//                     // Further assertions on expected data can be made here based on inserted data
+//                 }
+//                 Err(e) => panic!("Failed to SELECT with JOIN: {:?}", e),
+//             };
+//         }
+//         Err(e) => panic!("Failed to SELECT: {:?}", e),
+//     }
+// }
