15de7adb6d7d24b41989a800d1fc910d5bb4237c
Christopher Zeuch <chriszeuch.cz@gmail.com>
2024-10-07 20:52:03 +0200

    Moved structs into mod & updated test files

--- njord/tests/sqlite/open_test.rs
+++ njord/tests/sqlite/open_test.rs
@@ -1,46 +1,5 @@
-use njord::keys::{AutoIncrementPrimaryKey, PrimaryKey};
-use njord::sqlite::{self};
-use njord::table::Table;
-use njord_derive::Table;
+use njord::sqlite;
 use std::path::Path;
-
-#[derive(Table, Clone)]
-#[table_name = "users"]
-pub struct User {
-    id: AutoIncrementPrimaryKey<usize>,
-    username: String,
-    email: String,
-    address: String,
-}
-
-#[derive(Table)]
-#[table_name = "users"]
-pub struct UserWithSubQuery {
-    id: AutoIncrementPrimaryKey<usize>,
-    username: String,
-    email: String,
-    address: String,
-    additional_address: String,
-}
-
-#[derive(Table)]
-#[table_name = "categories"]
-pub struct Category {
-    id: PrimaryKey<usize>,
-    name: String,
-}
-
-#[derive(Table)]
-#[table_name = "products"]
-pub struct Product {
-    id: PrimaryKey<usize>,
-    name: String,
-    description: String,
-    price: f64,
-    stock_quantity: usize,
-    category: Category, // one-to-one relationship
-    discount: f64,
-}
 
 #[test]
 fn open_db() {
--- njord/tests/sqlite/select_test.rs
+++ njord/tests/sqlite/select_test.rs
@@ -1,49 +1,10 @@
 use njord::column::Column;
 use njord::condition::Condition;
-use njord::keys::{AutoIncrementPrimaryKey, PrimaryKey};
-use njord::sqlite::{self};
-use njord::table::Table;
-use njord_derive::Table;
+use njord::sqlite;
+use njord::keys::AutoIncrementPrimaryKey;
 use std::collections::HashMap;
 use std::path::Path;
-
-#[derive(Table, Clone)]
-#[table_name = "users"]
-pub struct User {
-    id: AutoIncrementPrimaryKey<usize>,
-    username: String,
-    email: String,
-    address: String,
-}
-
-#[derive(Table)]
-#[table_name = "users"]
-pub struct UserWithSubQuery {
-    id: AutoIncrementPrimaryKey<usize>,
-    username: String,
-    email: String,
-    address: String,
-    additional_address: String,
-}
-
-#[derive(Table)]
-#[table_name = "categories"]
-pub struct Category {
-    id: PrimaryKey<usize>,
-    name: String,
-}
-
-#[derive(Table)]
-#[table_name = "products"]
-pub struct Product {
-    id: PrimaryKey<usize>,
-    name: String,
-    description: String,
-    price: f64,
-    stock_quantity: usize,
-    category: Category, // one-to-one relationship
-    discount: f64,
-}
+use super::{User, UserWithSubQuery};
 
 #[test]
 fn select() {
