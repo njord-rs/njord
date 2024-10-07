00f3f0ffa81d619ab2ecb236333efe834ee52d29
Chase Willden <chase.willden@gmail.com>
2024-10-07 15:53:57 -0600

    Update the Join to be Arc, clean up query, hydrate select_join.db

--- njord/tests/sqlite/select_test.rs
+++ njord/tests/sqlite/select_test.rs
@@ -2,8 +2,12 @@ use njord::column::Column;
 use njord::condition::Condition;
 use njord::keys::{AutoIncrementPrimaryKey, PrimaryKey};
 use njord::sqlite;
+use njord::table::Table;
+use njord::util::JoinType;
+use njord_derive::Table;
 use std::collections::HashMap;
 use std::path::Path;
+use std::sync::Arc;
 
 #[derive(Table, Clone)]
 #[table_name = "users"]
@@ -22,6 +26,14 @@ pub struct UserWithSubQuery {
     email: String,
     address: String,
     additional_address: String,
+}
+
+#[derive(Table)]
+#[table_name = "users"]
+pub struct UsersWithJoin {
+    username: String,
+    price: f64,
+    name: String,
 }
 
 #[derive(Table)]
@@ -535,36 +547,40 @@ fn select_in() {
     };
 }
 
-// #[test]
-// fn select_join() {
-//     let db_relative_path = "./db/select_join.db";
-//     let db_path = Path::new(&db_relative_path);
-//     let conn = sqlite::open(db_path);
-//
-//     // Assume we have pre-inserted some data into the users and products tables
-//     let columns = vec![
-//         "users.username".to_string(),
-//         "products.name".to_string(),
-//         "products.price".to_string(),
-//     ];
-//
-//     // Assuming a hypothetical join condition: users.id = products.user_id
-//     let join_condition = Condition::Eq("users.id".to_string(), "products.user_id".to_string());
-//     match conn {
-//         Ok(c) => {
-//             let result = sqlite::select(&c, columns)
-//                 .from(User::default())
-//                 .join(JoinType::Inner, Category::default(), join_condition)
-//                 .build();
-//             match result {
-//                 Ok(r) => {
-//                     // Check the number of results and assert against expected values
-//                     assert!(!r.is_empty(), "Expected results, but got none.");
-//                     // Further assertions on expected data can be made here based on inserted data
-//                 }
-//                 Err(e) => panic!("Failed to SELECT with JOIN: {:?}", e),
-//             };
-//         }
-//         Err(e) => panic!("Failed to SELECT: {:?}", e),
-//     }
-// }
+#[test]
+fn select_join() {
+    let db_relative_path = "./db/select_join.db";
+    let db_path = Path::new(&db_relative_path);
+    let conn = sqlite::open(db_path);
+
+    // Assume we have pre-inserted some data into the users and products tables
+    let columns = vec![
+        Column::Text("users.username".to_string()),
+        Column::Text("products.name".to_string()),
+        Column::Text("products.price".to_string()),
+    ];
+
+    // Assuming a hypothetical join condition: users.id = products.user_id
+    let join_condition = Condition::Eq("users.id".to_string(), "products.user_id".to_string());
+    match conn {
+        Ok(c) => {
+            let result = sqlite::select(&c, columns)
+                .from(UsersWithJoin::default())
+                .join(
+                    JoinType::Inner,
+                    Arc::new(Product::default()),
+                    join_condition,
+                )
+                .build();
+            match result {
+                Ok(r) => {
+                    // Check the number of results and assert against expected values
+                    assert!(!r.is_empty(), "Expected results, but got none.");
+                    // Further assertions on expected data can be made here based on inserted data
+                }
+                Err(e) => panic!("Failed to SELECT with JOIN: {:?}", e),
+            };
+        }
+        Err(e) => panic!("Failed to SELECT: {:?}", e),
+    }
+}
