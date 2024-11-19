6625e29bea55fc51c426013015d38e2768b83c8e
Marcus Cveticanin <k01522@norsk-tipping.no>
2024-11-19 14:21:00 +0100

    Deleting old shell scripts

--- njord_examples/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# -----------------------------------------------------------------------------
-# Script: run.sh
-# Description: Run new migration schema changes with SQLite support.
-# Author: Marcus Cvjeticanin
-# Date: December 15, 2023
-# -----------------------------------------------------------------------------
-
-cargo run --manifest-path=../../njord_cli/Cargo.toml -- migration run -- --env=development --log-level=debug
